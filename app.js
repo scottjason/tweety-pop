ac23f1fe4523078b8de1a1ddf07f98b4223399cf
Scott Jason <scottleejason@gmail.com>
2014-08-06 23:56:54 -0700

    regenerates repo, hides api keys

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var express = require('express'),
     io = require('socket.io').listen(server),
     util = require('util'),
     twitter = require('twitter'),
-    sentiment = require('sentiment')
-
-// var wordTracker = "barack obama"
+    sentiment = require('sentiment'),
+    env = require('node-env-file')
 
 
 server.listen(3000);
@@ -23,21 +22,18 @@ app.get('/', function(req, res) {
 
 
 tweet = new twitter({
-    consumer_key: "Qz8vqLjcmgxOjhUpwd3hD2ZCw",
-    consumer_secret: "vRSxeLjj2pddubDxkpaZ1bqsonC0SrWsx9xMaBw91U2P8N42J2",
-    access_token_key: "195177239-1NI8bL9utZ2MnNXowy607mYLABlH83gp4k9TAgrA",
-    access_token_secret: "ZVusxwm9y4aJCnvtx3MHj7148REZikXyySeZURZsLUVGz"
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key,
+    access_token_secret: process.env.access_token_secret
 });
 
-
 io.sockets.on('connection', function() {
-    // var words = 'great american music hall, fillmore, bottom of the hill, ruby skye, asiasf, crazy horse, press club, supperclub, public works, city nights, presidio social club, cobbs comedy club, sloane, slow club, club quarters, smugglers cove, f8, vessel, bootie, academy of the scienes, monroe, popscene, icon ultra lounge, penthouse club, Mezzanine, hungry club'
-        var words = 'barack obama, obama'
+    var words = 'barack obama, obama'
     tweet.stream('statuses/filter', {
-        "language": 'en',
-        "track": words
-        // "locations": "-122.75,36.8,-121.75,37.8"
-    },
+            "language": 'en',
+            "track": words
+        },
 
         function(stream) {
             stream.on('data', function(data) {
