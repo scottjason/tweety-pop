344297404915e12be7021131756001f9bd2dd762
Scott Jason <scottleejason@gmail.com>
2014-08-13 01:02:58 -0700

    debugs for heroku deployment

--- app.js
+++ app.js
@@ -79,8 +79,8 @@ tweet = new twitter({
       newTweet = decodeURIComponent(escape(foreignCharacters));
 
         if (data.id != null) {
-          // var newScore = new Rating ( { popStar: newTweet, tweetScore: sentiment(newTweet).score } );
-          // newScore.save(function(err) { if (err) { throw err }
+          var newScore = new Rating ( { popStar: newTweet, tweetScore: sentiment(newTweet).score } );
+          newScore.save(function(err) { if (err) { throw err }
           io.sockets.emit('message', newTweet, sentiment(newTweet).score);
      });
     }
