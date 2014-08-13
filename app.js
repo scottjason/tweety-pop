1aae65f5751eb2b05b2e99af51218c4d653cac34
Scott Jason <scottleejason@gmail.com>
2014-08-12 17:14:52 -0700

    adds new relic

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // require modules
+
 require('newrelic')
+
+exports.config = {
+  app_name: ['tweet-pop'],
+  license_key : "c683d0374bbb87a8e5caf3c31232f8fc0afdaa8c",
+  logging: {
+    level:'info'
+  }
+}
 var Array = require('node-array');
 var express = require('express'),
     app = express(),
