7c123051460fb902dc7bbb5cf856144f9940e4ee
Scott Jason <scottleejason@gmail.com>
2014-08-14 15:18:44 -0700

    fixes stream lag

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ tweet = new twitter({
 });
 
 // stream incoming tweets, write to database, emit to client
-io.sockets.on('connection', function() {
+
   tweet.stream('statuses/filter', { "track": popTracker },
     function(stream) {
       stream.on('data', function(data) {
@@ -89,8 +89,9 @@ io.sockets.on('connection', function() {
    });
   });
 
-  reOpenDbStream();
+io.sockets.on('connection', function() {
       // stream the database, emit to client
+      reOpenDbStream();
       function reOpenDbStream(){
       var streamdB = Rating.find().stream();
       streamdB.on('data', function(doc)  {
