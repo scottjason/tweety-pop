0bc6278613ae5a91e3a7107369a026c42eda8d4c
Scott Jason <scottleejason@gmail.com>
2014-08-22 21:49:34 -0700

    restructures server side, adds performance enhancements

--- app.js
+++ app.js
@@ -26,81 +26,56 @@ var swiftScores = [];
 var timberlakeScores = [];
 var lovatoScores = [];
 
-// initiates server connection
-var port = process.env.PORT || 3000;
-server.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Express configuration
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // declares public folder
 app.use('/', express.static(__dirname + '/public'));
-
 // declares routes
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
-
-// initiate socket connection, query database, emit results to client on query completion
-io.sockets.on('connection', function(socket) {
-    // queries the database in chunks of 100 collections
-    var tweetQuery = Rating.find( {} ).limit(100);
-    tweetQuery.exec(function(err, docs) {
-
-    // emit database data to client
-    socket.emit('queryLoaded', docs)
-
-    // Handle disconnects
-    socket.on('disconnect', function(docs) {
-    session.disconnect(io, socket, docs);
-   });
-  });
- });
+res.sendfile(__dirname + '/index.html');
 });
 
-// creates database schema
-var tweetSchema = mongoose.Schema({
-  popStar: {
-    type: String
-  },
-  tweetScore: {
-    type: Number
-  }
-}, {
-  capped: {
-    size: 2000000,
-    max: 10000,
-    autoIndexId: true
-  }
-});
-
-// creates model Rating and 'score' collection
-var Rating = mongoose.model('score', tweetSchema);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Models & Database Initiation
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // initiates database connection options
-var options = {
-  server: {
-    socketOptions: {
-      keepAlive: 1,
-      connectTimeoutMS: 30000
-    }
-  },
-  replset: {
-    socketOptions: {
-      keepAlive: 1,
-      connectTimeoutMS: 30000
-    }
-  }
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
 };
 
 // initiates database connection
-mongoose.connect("mongodb://scottjason:tweetypop084@ds033559.mongolab.com:33559/heroku_app28482092", function(err) {
+mongoose.connect("mongodb://scottjason:tweetypop084@ds033559.mongolab.com:33559/heroku_app28482092", options, function(err) {
   if (!err) {
     console.log("Successfully initiated database connection.");
   }
 });
 
+// creates database schema
+var tweetSchema = mongoose.Schema({
+  popStar: { type: String }, tweetScore: { type: Number } },
+{
+  capped: { size: 2000000, max: 10000, autoIndexId: true }
+});
 
+// creates model Rating and 'score' collection
+var Rating = mongoose.model('score', tweetSchema);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Server
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// initiates server connection
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+  console.log("Tweety Pop successfully listening on " + port);
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Client Handling
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // twitter authorization
 tweet = new twitter({
@@ -110,6 +85,7 @@ tweet = new twitter({
   access_token_secret: "ZVusxwm9y4aJCnvtx3MHj7148REZikXyySeZURZsLUVGz"
 });
 
+io.sockets.on('connection', function (socket) {
 // streams incoming tweets
 tweet.stream('statuses/filter', {
     "track": popTracker
@@ -121,66 +97,69 @@ tweet.stream('statuses/filter', {
       var newTweet = data.text;
       var foreignCharacters = unescape(encodeURIComponent(newTweet));
       newTweet = decodeURIComponent(escape(foreignCharacters));
+      var score = sentiment(newTweet).score
+      if (newTweet != null && score != 0) {
+          var newScore = new Rating( { popStar: newTweet, tweetScore: score } )
 
-      if (newTweet != null) {
-        var score = sentiment(newTweet).score
-
-        if (score != 0) {
-          var newScore = new Rating({
-            popStar: newTweet,
-            tweetScore: score
-          })
           newScore.save(function(err) {
-            if (err) {
-              throw err
-            }
-          })
-        }
+            if (err) { throw err } })
 
         analyzeTweet(newTweet, score)
         io.sockets.emit('message', newTweet, score)
       }
     })
-
-    function analyzeTweet(doc, score) {
-      if (doc.indexOf('perry') != -1 && score != 0) {
-        perryScores.push(score);
-        io.sockets.emit('perryScoreArray', perryScores);
-      } else if (doc.indexOf('bieber') != -1 && score != 0) {
-        bieberScores.push(score);
-        io.sockets.emit('bieberScoreArray', bieberScores);
-      } else if ((doc.indexOf('levine') != -1 || doc.indexOf('maroon') != -1) && score != 0) {
-        levineScores.push(score);
-        io.sockets.emit('levineScoreArray', levineScores);
-      } else if (doc.indexOf('beyonce') != -1 && score != 0) {
-        beyonceScores.push(score);
-        io.sockets.emit('beyonceScoreArray', beyonceScores);
-      } else if (doc.indexOf('rihanna') != -1 && score != 0) {
-        rihannaScores.push(score);
-        io.sockets.emit('rihannaScoreArray', rihannaScores);
-      } else if (doc.indexOf('eminem') != -1 && score != 0) {
-        eminemScores.push(score);
-        io.sockets.emit('eminemScoreArray', eminemScores);
-      } else if (doc.indexOf('miley') != -1 && score != 0) {
-        mileyScores.push(score);
-        io.sockets.emit('mileyScoreArray', mileyScores);
-      } else if (doc.indexOf('kanye') != -1 && score != 0) {
-        kanyeScores.push(score);
-        io.sockets.emit('kanyeScoreArray', kanyeScores);
-      } else if (doc.indexOf('gaga') != -1 && score != 0) {
-        gagaScores.push(score);
-        io.sockets.emit('gagaScoreArray', gagaScores);
-      } else if (doc.indexOf('swift') != -1 && score != 0) {
-        swiftScores.push(score);
-        io.sockets.emit('swiftScoreArray', swiftScores);
-      } else if (doc.indexOf('timberlake') != -1 && score != 0) {
-        timberlakeScores.push(score);
-        io.sockets.emit('timberlakeScoreArray', timberlakeScores);
-      } else if (doc.indexOf('lovato') != -1 && score != 0) {
-        lovatoScores.push(score);
-        io.sockets.emit('lovatoScoreArray', lovatoScores);
-      } else {
-        console.log('.. analyzing data stream ..')
-      }
-    }
   })
+
+// queries the database in chunks of 100 collections
+var tweetQuery = Rating.find( {} ).limit(100);
+  tweetQuery.exec(function(err, docs) {
+    if(err) throw new Error( 'There was an error while retrieving instructions from the database.' );
+    // emit database data to client
+    io.sockets.emit('queryLoaded', docs)
+  });
+
+  function analyzeTweet(doc, score) {
+    if (doc.indexOf('perry') != -1 && score != 0) {
+      perryScores.push(score);
+      io.sockets.emit('perryScoreArray', perryScores);
+    } else if (doc.indexOf('bieber') != -1 && score != 0) {
+      bieberScores.push(score);
+      io.sockets.emit('bieberScoreArray', bieberScores);
+    } else if ((doc.indexOf('levine') != -1 || doc.indexOf('maroon') != -1) && score != 0) {
+      levineScores.push(score);
+      io.sockets.emit('levineScoreArray', levineScores);
+    } else if (doc.indexOf('beyonce') != -1 && score != 0) {
+      beyonceScores.push(score);
+      io.sockets.emit('beyonceScoreArray', beyonceScores);
+    } else if (doc.indexOf('rihanna') != -1 && score != 0) {
+      rihannaScores.push(score);
+      io.sockets.emit('rihannaScoreArray', rihannaScores);
+    } else if (doc.indexOf('eminem') != -1 && score != 0) {
+      eminemScores.push(score);
+      io.sockets.emit('eminemScoreArray', eminemScores);
+    } else if (doc.indexOf('miley') != -1 && score != 0) {
+      mileyScores.push(score);
+      io.sockets.emit('mileyScoreArray', mileyScores);
+    } else if (doc.indexOf('kanye') != -1 && score != 0) {
+      kanyeScores.push(score);
+      io.sockets.emit('kanyeScoreArray', kanyeScores);
+    } else if (doc.indexOf('gaga') != -1 && score != 0) {
+      gagaScores.push(score);
+      io.sockets.emit('gagaScoreArray', gagaScores);
+    } else if (doc.indexOf('swift') != -1 && score != 0) {
+      swiftScores.push(score);
+      io.sockets.emit('swiftScoreArray', swiftScores);
+    } else if (doc.indexOf('timberlake') != -1 && score != 0) {
+      timberlakeScores.push(score);
+      io.sockets.emit('timberlakeScoreArray', timberlakeScores);
+    } else if (doc.indexOf('lovato') != -1 && score != 0) {
+      lovatoScores.push(score);
+      io.sockets.emit('lovatoScoreArray', lovatoScores);
+    } else {
+      console.log('.. analyzing data stream ..')
+    }
+  }
+  socket.on('disconnect', function() {
+  console.log('Tweety Pop has been temporarily disconnected.');
+  });
+});
