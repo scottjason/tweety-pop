41092f601b40d81108fcdae1aca63c2e66fe9453
scottjason <scottleejason@gmail.com>
2014-09-04 23:41:55 -0700

    adds db connection events, configures app

--- init.js
+++ init.js
@@ -1,11 +1,5 @@
 ;(function initialize(){
 var app = require('./server/controllers/app.js')
-  , database = require('./server/controllers/database.js')
-  , stream = require('./server/models/stream.js')
-  , stream = require('./server/models/save.js');
-
     app.initialize();
-    app.listen();
-    database.initialize();
-    database.connect();
 })()
+
--- server/controllers/app.js
+++ server/controllers/app.js
@@ -1,6 +1,10 @@
 var express = require('express')
   , app = express()
-  , server = require('http').createServer(app)
+  , server = require('http').createServer( app )
+  , dotenv = require('dotenv')
+  , mongoose = require('mongoose');
+    dotenv.load();
+
 
 module.exports = {
   initialize: function() {
@@ -13,14 +17,57 @@ module.exports = {
       });
     })
 
-    app.get('/', function(req, res) {
+    app.get('/', function( req, res ) {
       res.sendFile(__dirname + '/index.html');
     });
+    this.listen();
   },
   listen: function() {
+    io = require('socket.io').listen( server )
+
     var port = process.env.PORT || 3000
     server.listen(port, function() {
-      console.log("Tweety Pop successfully listening on " + port);
+      console.log( "Tweety Pop successfully listening on " + port );
     });
+    this.connect();
+  },
+  connect: function(){
+    var mongodbUri = process.env.mongodbUri
+    mongoose.connect(mongodbUri);
+    this.db = mongoose.connection
+
+    this.db.on('open', function() {
+      console.log('Mongoose default connection open to ' + mongodbUri);
+    });
+
+    this.db.on('error', function(err) {
+      console.log('Mongoose default connection error: ' + err);
+    });
+
+    this.db.on('disconnected', function() {
+      console.log('Mongoose default connection disconnected');
+    });
+
+    process.on('SIGINT', function() {
+      console.log(this)
+      this.db.close(function() {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+      });
+    }.bind(this));
+    this.create();
+  },
+  create: function(){
+    tweetSchema = mongoose.Schema({
+      popStar: { type: String }, tweetScore: { type: Number } },
+      { capped: { size: 50000, max: 50000, autoIndexId: false }
+    });
+    Artist = mongoose.model('artist', tweetSchema);
   }
 }
+
+
+
+
+
+
