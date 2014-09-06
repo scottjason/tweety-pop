3e8ab3eaecbd610f610803e110dbbe5a82f5cf7f
scottjason <scottleejason@gmail.com>
2014-09-05 19:51:54 -0700

    modifies file naming conventions

--- init.js
+++ init.js
@@ -9,11 +9,10 @@
     app.use('/', express.static( __dirname + '/public' ));
 
     app.get('/', function( req, res ) {
-      res.sendFile(__dirname + '/index.html');
+      res.sendFile( __dirname + '/index.html' );
     });
 
     database.initialize( server, port, io );
-
 })()
 
 
