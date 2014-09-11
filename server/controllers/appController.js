6c6e8c1d3c461c61377999a363c874a8a2646937
scottjason <scottleejason@gmail.com>
2014-09-10 17:20:21 -0700

    adds self-invoking query function

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,4 @@
-var every = require('schedule').every
-  , Artist = require('../models/dbModel.js');
+Artist = require('../models/dbModel.js');
 
 module.exports = {
   initialize: function( io ) {
@@ -13,63 +12,20 @@ module.exports = {
   },
   save: function( content, score ) {
     var newTweet = new Artist ( { popStar: content, tweetScore: score } );
-        newTweet.save(function( err ) { if ( err ) { console.log( err ) }
+        newTweet.save(function( err ) { if ( err ) { console.error( err ) }
     })
   },
-  fetchPerry: function() {
-
-    every('2s').do(function() {
-    var tweetQuery = Artist.find({popStar: /perry/}).limit(25);
-        tweetQuery.exec(function( err, docs ) {
-    if (err) return console.error( err );
+  query: function() {
+  Artist.find({}).limit(50).exec(function( err, docs ) {
+    if ( err ) return console.error( err );
       for (var i = 0; i < docs.length; i++) {
-
-        this.analyze( docs[i].popStar, docs[i].tweetScore )
+    this.analyze( docs[i].popStar, docs[i].tweetScore )
      }
-   }.bind( this ))
-  }.bind( this ))
- },
- fetchLevine: function(){
-
-    every('2s').do(function() {
-    var tweetQuery = Artist.find({popStar: /levine/}).limit(25);
-        tweetQuery.exec(function( err, docs ) {
-    if (err) return console.error( err );
-      for (var i = 0; i < docs.length; i++) {
-
-        this.analyze( docs[i].popStar, docs[i].tweetScore )
-     }
-   }.bind( this ))
-  }.bind( this ))
- },
-  fetchBeyonce: function(){
-
-    every('2s').do(function() {
-    var tweetQuery = Artist.find({popStar: /beyonce/}).limit(25);
-        tweetQuery.exec(function( err, docs ) {
-    if (err) return console.error( err );
-      for (var i = 0; i < docs.length; i++) {
-
-        this.analyze( docs[i].popStar, docs[i].tweetScore )
-     }
-   }.bind( this ))
-  }.bind( this ))
- },
- fetchBieber: function(){
-
-    every('2s').do(function() {
-    var tweetQuery = Artist.find({popStar: /bieber/}).limit(25);
-        tweetQuery.exec(function( err, docs ) {
-    if (err) return console.error( err );
-      for (var i = 0; i < docs.length; i++) {
-
-        this.analyze( docs[i].popStar, docs[i].tweetScore )
-     }
-   }.bind( this ))
-  }.bind( this ))
- },
+   }.bind( this ));
+     setTimeout( this.query.bind( this ) , 3000 );
+  },
    handler: function( error ){
-    console.log( error )
+    console.error( error );
  }
 }
 
